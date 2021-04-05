962cf9aa21da9c36590d2c88892e653e94c1c559
Rishabh <rishabhrathi000@gmail.com>
2021-04-05 17:43:23 +0530

    added docs

--- DiseasePredictor/settings.py
+++ DiseasePredictor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@c6&ejd3&so_3&8be2*e455di($h%yy93u!_56g5f71195@waj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # added manually
     'diabetes',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -125,5 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+# Managing media
+STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles")
+
 WHITENOISE_USE_FINDERS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
