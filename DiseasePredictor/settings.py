48a1a09aef9ae742131add8bfb70c5b4cf4a570b
Rishabh <rishabhrathi000@gmail.com>
2021-04-06 23:11:58 +0530

    cors

--- DiseasePredictor/settings.py
+++ DiseasePredictor/settings.py
@@ -58,10 +58,10 @@ MIDDLEWARE = [
 ]
 
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+# ]
 
 
 ROOT_URLCONF = 'DiseasePredictor.urls'
